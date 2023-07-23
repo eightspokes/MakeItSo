ad7c7ec9f68cbc5bd18e7d345c7bf6f28066551a
romanmuni8 <romanmuni8@gmail.com>
2023-07-23 14:32:28 -0400

    Initial Commit

---
+++ MakeItSo/MakeItSoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItSoApp.swift
+//  MakeItSo
+//
+//  Created by Roman on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MakeItSoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
